e57a22655aeb9fa424bc0a3b487be16fece50a5e
haniyeh-habibi <haniye.habibi@gmail.com>
2025-12-09 13:50:22 +0000

    chore: simplify highlight animation by removing intermediate keyframes

--- src/theme/animations.ts
+++ src/theme/animations.ts
@@ -5,14 +5,6 @@ export const createHighlightAnimation = (theme: Theme) => keyframes`
     background-color: ${alpha(theme.palette.success.main, 0.15)};
     border-left: 6px solid ${theme.palette.success.main};
   }
-  15% {
-    background-color: ${alpha(theme.palette.success.main, 0.08)};
-    border-left: 4px solid ${theme.palette.success.main};
-  }
-  85% {
-    background-color: ${alpha(theme.palette.success.main, 0.08)};
-    border-left: 4px solid ${theme.palette.success.main};
-  }
   100% {
     background-color: ${alpha(theme.palette.success.main, 0.04)};
     border-left: 0px solid transparent;
